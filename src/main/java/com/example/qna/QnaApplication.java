f570bff590d9fbd3cd1856e0ff7a4c30ad1da957
gyuyoungp <gy971224@gmail.com>
2021-09-04 15:43:41 +0900

    tomcat build 오류 fix

--- src/main/java/com/example/qna/QnaApplication.java
+++ src/main/java/com/example/qna/QnaApplication.java
@@ -2,12 +2,18 @@ package com.example.qna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QnaApplication {
+public class QnaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QnaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QnaApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(QnaApplication.class);
+    }
 }
